9a93a4c414bb7c38da771c968eaa947d252dfea9
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-12-24 20:25:36 +0500

    About Us

--- app/src/main/java/com/RDM/TourSum/MainActivity.kt
+++ app/src/main/java/com/RDM/TourSum/MainActivity.kt
@@ -55,8 +55,10 @@ class MainActivity : AppCompatActivity() {
         applyPrimaryColor(colorHex)
 
         // Navigation drawer setup
-        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
-            R.string.nav_open, R.string.nav_close)
+        toggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout, binding.toolbar,
+            R.string.nav_open, R.string.nav_close
+        )
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
@@ -84,7 +86,10 @@ class MainActivity : AppCompatActivity() {
         val primaryColor = try {
             Color.parseColor(colorHex)
         } catch (e: IllegalArgumentException) {
-            ContextCompat.getColor(this, R.color.primaryColor) // Fallback to a default color resource
+            ContextCompat.getColor(
+                this,
+                R.color.primaryColor
+            ) // Fallback to a default color resource
         }
 
         // Apply the color to the toolbar, status bar, and navigation drawer
@@ -102,82 +107,3 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 }
-
-
-
-//package com.RDM.TourSum
-//
-//import android.os.Build
-//import android.os.Bundle
-//import androidx.annotation.RequiresApi
-//import androidx.appcompat.app.ActionBarDrawerToggle
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.fragment.app.Fragment
-//import androidx.fragment.app.FragmentTransaction
-//import androidx.lifecycle.lifecycleScope
-//import com.RDM.TourSum.data.LocalDatabase
-//import com.RDM.TourSum.databinding.ActivityMainBinding
-//import com.RDM.TourSum.repository.FirebaseRepository
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.launch
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    private lateinit var binding: ActivityMainBinding
-//    private lateinit var toggle: ActionBarDrawerToggle
-//    private lateinit var firebaseRepo: FirebaseRepository
-//
-//    @RequiresApi(Build.VERSION_CODES.S)
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        // Firebase sync
-//        val bookingDao = LocalDatabase.getDatabase(this).bookingDao()
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            firebaseRepo = FirebaseRepository(bookingDao)
-//            firebaseRepo.syncMissedRecords()
-//            bookingDao.deleteAllSyncedRecords()
-//        }
-//
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        // Fetch webName from SharedPreferences
-//        val sharedPreferences = getSharedPreferences("UserPreferences", MODE_PRIVATE)
-//        val webName = sharedPreferences.getString("webName", "Default WebName") // Fallback to "Default WebName"
-//        // Set up toolbar
-//        setSupportActionBar(binding.toolbar)
-//        supportActionBar?.title = webName // Set the toolbar title to webName
-//
-//        // Navigation drawer setup
-//        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
-//            R.string.nav_open, R.string.nav_close)
-//        binding.drawerLayout.addDrawerListener(toggle)
-//        toggle.syncState()
-//
-//        // Handle navigation item selection
-//        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.about_us -> {
-//                    openFragment(AboutUsFragment())
-//                }
-//
-//                R.id.invoiceHistory -> {
-//                    openFragment(InvoiceHistoryFragment())
-//                }
-//            }
-//            binding.drawerLayout.closeDrawers()
-//            true
-//        }
-//
-//        // Open HomeFragment by default
-//        openFragment(HomeFragment())
-//    }
-//
-//    private fun openFragment(fragment: Fragment) {
-//        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-//        fragmentTransaction.replace(R.id.content_frame, fragment)
-//        fragmentTransaction.commit()
-//    }
-//}
-//
