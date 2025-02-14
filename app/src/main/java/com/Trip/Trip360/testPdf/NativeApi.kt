f90268e1e6b4fdda2ae43286a946cbd28b494a4c
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2025-02-14 11:01:07 +0500

    About Us

--- app/src/main/java/com/Trip/Trip360/testPdf/NativeApi.kt
+++ app/src/main/java/com/Trip/Trip360/testPdf/NativeApi.kt
@@ -37,6 +37,7 @@ object NativeApi {
 
         (view.findViewById<TextView>(R.id.tvGuestName)).text = booking.name
         (view.findViewById<TextView>(R.id.tvPickupTime)).text = booking.pickupTime
+        (view.findViewById<TextView>(R.id.tvPhoneNo)).text = booking.phone
         (view.findViewById<TextView>(R.id.tvBookingDate)).text = booking.pickupDate
         (view.findViewById<TextView>(R.id.tvGrandTotal)).text = "AED " + calculateTotalPrice(booking)
         (view.findViewById<TextView>(R.id.tvPackageName)).text = booking.packageName ?: "N/A"
@@ -161,6 +162,7 @@ object NativeApi {
         (view.findViewById<TextView>(R.id.tvGuestName)).text = booking.name
         (view.findViewById<TextView>(R.id.tvPickupTime)).text = booking.pickupTime
         (view.findViewById<TextView>(R.id.tvBookingDate)).text = booking.pickupDate
+        (view.findViewById<TextView>(R.id.tvPhoneNo)).text = booking.phone
         (view.findViewById<TextView>(R.id.tvGrandTotal)).text = "AED " + calculateTotalPrice(booking)
         (view.findViewById<TextView>(R.id.tvPackageName)).text = booking.packageName ?: "N/A"
         (view.findViewById<TextView>(R.id.tvPickupLocation)).text = booking.pickupLocation ?: "N/A"
